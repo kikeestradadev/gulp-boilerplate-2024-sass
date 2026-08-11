03a0bc4f54ddad51fa2cc1da3ae9915388a101a5
kikeestradadev <125921116+kikeestradadev@users.noreply.github.com>
2026-08-11 16:08:22 -0600

    clean repo

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-import internalModule from './modules/internalModule';
-import Prism from 'prismjs';
-(() => {
-	internalModule();
-	Prism.highlightAll(); // Corrected: Use Prism.highlightAll() instead of undefined prismjs()
-})();
